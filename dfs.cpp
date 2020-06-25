468a90123ccac7f3317f74bf8acdf8f02bc2a5c5
Megham-Garg <33658001+Megham-Garg@users.noreply.github.com>
2020-06-25 07:49:35 +0530

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_VERT 100
+void dfs(map<char, vector<char>> adList, map<char, bool> &ver, char node){
+    if(!ver[node]) {cout << node << " "; ver[node] = true;}
+    if(adList.find(node) == adList.end()) return;
+    for(auto it = adList[node].begin(); it!=adList[node].end(); it++){
+        dfs(adList, ver, *it);
+    }
+}
+int main(){
+    map<char, vector<char>> adList; // adjacency list for graph
+    map<char, bool> ver;            // vertices present in graph and their visited status
+    int e;  cin >> e;                         // number of edges
+    char start_node;
+    for(int i = 0; i < e; i++){
+        char u,v; cin >> u >> v;
+        if(i==0) start_node = u;
+        if(ver.find(u) == ver.end()) ver[u] = false;
+        if(ver.find(v) == ver.end()) ver[v] = false;
+        if(adList.find(u) == adList.end()) {vector<char> adj; adj.push_back(v); adList[u] = adj;}
+        else adList[u].push_back(v);
+    }
+    // for(auto it = adList.begin(); it!= adList.end(); it++){
+    //     cout << it->first << " : ";
+    //     for(auto it2 = it->second.begin(); it2!=it->second.end(); it2++) cout << *it2 << " ";
+    //     cout << endl;
+    // }
+    dfs(adList, ver, start_node);
+    return 0;
+}
