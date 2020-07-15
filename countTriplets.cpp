f590514170a72876a07e1e82373faa15c64683c9
Megham-Garg <33658001+Megham-Garg@users.noreply.github.com>
2020-07-15 15:18:03 +0530

    Create countTriplets.cpp

---
+++ countTriplets.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int t = 0; cin >> t;
+	while(t--){
+	    int n; cin >> n;
+	    vector<int> a(n);
+	    int ans = 0;
+	    for(int i = 0; i < n; i++) {int x; cin >> x; a[i]=x;}
+	    sort(a.begin(),a.end());
+	    for(int i = n-1; i > 1; i--){
+	        int j = 0, k = i-1;
+	        while(j < k){
+	            if(a[j]+a[k]==a[i]) ans++;
+	            if(a[j]+a[k]>a[i]) k--;
+	            else j++;
+	        }
+	    }
+	    cout << (ans?ans:-1) << "\n";
+	}
+	return 0;
+}
