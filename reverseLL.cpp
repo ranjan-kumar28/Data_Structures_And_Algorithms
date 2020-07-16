8366e5dcc51571be817ca348d06fbf1ff9733b7e
Megham-Garg <33658001+Megham-Garg@users.noreply.github.com>
2020-07-16 17:59:43 +0530

    Create reverseLL.cpp

---
+++ reverseLL.cpp
@@ -0,0 +1,15 @@
+void reverse(Node* head) 
+{ 
+    Node* current = head; 
+    Node *prev = NULL, *next = NULL; 
+
+    while (current != NULL) {
+        next = current->next; 
+        current->next = prev; 
+        prev = current; 
+        current = next; 
+    } 
+    head = prev;
+    return head;
+} 
+  
