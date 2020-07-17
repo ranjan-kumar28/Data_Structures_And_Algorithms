1d7491d6a3098fd1e40608225d3d8776b90cc5f6
Megham-Garg <33658001+Megham-Garg@users.noreply.github.com>
2020-07-17 15:48:19 +0530

    Create BinaryTreeDLL.cpp

---
+++ BinaryTreeDLL.cpp
@@ -0,0 +1,20 @@
+void inorder(Node* root, vector<int> &v){
+    if(root->left) inorder(root->left, v);
+    v.push_back(root->data);
+    if(root->right) inorder(root->right, v);
+}
+Node * bToDLL(Node *root)
+{
+    Node *temp, *dll;
+    vector<int> v; inorder(root, v);
+    for(ele : v){
+        if(!dll) {dll = newNode(ele); temp = dll;}
+        else{
+            Node* next = newNode(ele);
+            next->left = dll;
+            dll->right = next;
+            dll = next;
+        }
+    }
+    return temp;
+}
