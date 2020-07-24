b0d68d0c788e9475c0d516cdffca086f1708795e
Megham-Garg <33658001+Megham-Garg@users.noreply.github.com>
2020-07-24 15:38:02 +0530

    Create shortestCommonSupersequence.cpp

---
+++ shortestCommonSupersequence.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int t = 0; cin >> t;
+	while(t--){
+	    int n,m;
+	    string a,b; cin >> a >> b;
+        n = int(a.size())+1;
+        m = int(b.size())+1;
+	    int dp[n][m];
+	    for(int i = 0; i < n; i++)
+	        for(int j = 0; j < m; j++) dp[i][j]=0;
+	        
+	    for(int i = 1; i < n; i++)
+	        for(int j = 1; j < m; j++){
+	            if(a[i-1] == b[j-1]) dp[i][j]=1+dp[i-1][j-1];
+	            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+	        }
+	    cout << n+m-dp[n-1][m-1]-2 << "\n";
+	}
+	return 0;
+}
