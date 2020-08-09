f5afe1031a209fac770298da16f24493699d1d4c
Megham-Garg <33658001+Megham-Garg@users.noreply.github.com>
2020-08-09 06:14:55 +0530

    Create mirrorTrees.cpp

---
+++ mirrorTrees.cpp
@@ -0,0 +1,8 @@
+void mirror(Node* node) {
+    if(!node) return;
+    Node* temp = node->left;
+    node->left = node->right;
+    node->right = temp;
+    mirror(node->left);
+    mirror(node->right);
+}
